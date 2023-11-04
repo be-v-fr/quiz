1d66c8b2d9eaa3b34747111c3b97dd6a46beaf9d
Bengt Früchtenicht <bengt.fruechtenicht@gmx.de>
2023-11-04 08:14:13 +0100

    added mechanics

--- script.js
+++ script.js
@@ -28,16 +28,26 @@ let questions = [
 ]
 
 let currentQuestion = 0;
+let correctAnswers = 0;
 
 function init() {
-    renderQuestion(0);
+    renderNumberOfQuestions();
+    showQuestion();
 }
 
-function renderQuestion() {
+function renderNumberOfQuestions() {
+    const numberOfQuestions = document.getElementById('numberOfQuestions');
+    numberOfQuestions.innerHTML = questions.length;
+}
+
+function showQuestion() {
+    const currentQuestionNumber = document.getElementById('currentQuestion');
     const question = document.getElementById('question');
+
+    currentQuestionNumber.innerHTML = currentQuestion + 1;
     question.innerHTML = `${questions[currentQuestion]['question']}`;
 
-    for (let i = 1; i < 5; i++) {
+    for (let i = 1; i < 5; i++) { // Schleife wird für jede Antwort durchlaufen
         const answer = document.getElementById(`answer${i}`);
         answer.classList.remove('bg-success');
         answer.classList.remove('bg-danger');
@@ -45,12 +55,46 @@ function renderQuestion() {
     }
 }
 
+function showResults() {
+    const quizCard = document.getElementById('quizCard');
+    const resultsCard = document.getElementById('resultsCard');
+    const result = document.getElementById('result');
+
+    quizCard.classList.add('dNone');
+    resultsCard.classList.remove('dNone');
+
+    result.innerHTML = resultHTML();
+}
+
 function checkAnswer(selectedAnswerIndex) {
     const selectedAnswer = document.getElementById(`answer${selectedAnswerIndex}`);
     const correctAnswerIndex = questions[currentQuestion]['correctAnswer'];
     const correctAnswer = document.getElementById(`answer${correctAnswerIndex}`);
+    const btn = document.getElementById('nextQuestionBtn');
+
     if (selectedAnswerIndex != correctAnswerIndex) {
         selectedAnswer.classList.add('bg-danger');
+    } else {
+        correctAnswers++;
     }
     correctAnswer.classList.add('bg-success');
+    btn.disabled = false;
+}
+
+function nextQuestion() {
+    const btn = document.getElementById('nextQuestionBtn');
+
+    currentQuestion++;
+    if (currentQuestion < questions.length) {
+        btn.disabled = true;
+        showQuestion();
+    } else {
+        showResults();
+    }
+}
+
+function resultHTML() {
+    return /* html */ `
+        ${correctAnswers} / ${questions.length}
+    `;
 }
